0a42e585c6e316aa3212bb768eb7a620dec51a4a
Aak2k3 <me.aakash@gmail.com>
2024-12-11 22:35:58 +0530

    geolocation support added

--- weather.js
+++ weather.js
@@ -11,9 +11,12 @@ const humidity = document.getElementById('humidity');
 const windSpeed = document.getElementById('wind-speed');
 const weatherIcon = document.getElementById('weather-icon');
 const errorMessage = document.getElementById('error-message');
+const currentLocation = document.getElementById('currentBtn');
+
 
 // Fetch Weather Data
 async function fetchWeather(city) {
+  let API_KEY = `9be20a0a88ddfe5a3ee4d50399f89adf`;
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
@@ -30,6 +33,37 @@ async function fetchWeather(city) {
     weatherDisplay.classList.add('hidden');
   }
 }
+
+//Fetching Current Location Weather 
+
+currentLocation.addEventListener('click', function(){
+  if(navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(async (position)=>{
+    const { latitude, longitude } = position.coords;
+
+    try {                    //callback Async/await
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+      );
+
+      if (!response.ok) throw new Error('Unable to fetch weather for current location');
+      const data = await response.json();
+
+      updateWeatherUI(data);
+
+    } catch (error) {
+      errorMessage.textContent = error.message;
+      errorMessage.classList.remove('hidden');
+      weatherDisplay.classList.add('hidden');
+    } 
+  });
+}else {
+      errorMessage.textContent = "Geolocation is not supported in your browser";
+      errorMessage.classList.remove('hidden');
+    }
+    
+  
+  });
 
 // Update Weather UI
 function updateWeatherUI(data) {
